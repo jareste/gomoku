55b3ad8afe35e9bda2b8ca8daa5a3d985ecd167c
Joan Areste Gonzalez <jareste2000@gmail.com>
2024-02-16 12:09:54 +0100

    printing map with iced

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,47 @@
-use egui::{CtxRef, CentralPanel};
-use egui_winit_platform::{Platform, PlatformDescriptor};
-use winit::event_loop::{EventLoop};
-use winit::window::WindowBuilder;
+// use iced::{button, Button, Column, Command, Element, Sandbox, Settings, Text};
 
-fn main() {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-    let platform = Platform::new(PlatformDescriptor {
-        physical_width: window.inner_size().width,
-        physical_height: window.inner_size().height,
-        scale_factor: window.scale_factor(),
-        font_definitions: Default::default(),
-        style: Default::default(),
-    });
+// #[derive(Default)]
+// pub struct MyApp {
+//     button_state1: button::State,
+//     button_state2: button::State,
+// }
 
-    event_loop.run(move |event, _, control_flow| {
-        // platform.handle_event(&event);
-        match event {
-            winit::event::Event::RedrawRequested(_) => {
-                let mut egui_ctx = CtxRef::default();
-                egui_ctx.begin_frame(egui::RawInput::default());
+// #[derive(Debug, Clone)]
+// pub enum MyMessage {
+//     Button1Clicked,
+//     Button2Clicked,
+// }
 
-                egui::TopBottomPanel::top("top_panel").show(&egui_ctx, |ui| {
-                    ui.horizontal(|ui| {
-                        ui.label("Gomoku Game");
-                    });
-                });
+// impl Sandbox for MyApp {
+//     type Message = MyMessage;
 
-                CentralPanel::default().show(&egui_ctx, |ui| {
-                    ui.vertical(|ui| {
-                        if ui.button("Button 1").clicked() {
-                            // Do nothing
-                        }
-                        if ui.button("Button 2").clicked() {
-                            // Do nothing
-                        }
-                        if ui.button("Button 3").clicked() {
-                            // Do nothing
-                        }
-                    });
-                });
+//     fn new() -> Self {
+//         Self::default()
+//     }
 
-                let (output, paint_commands) = egui_ctx.end_frame();
-                // platform.end_frame(Some(&window));                // Handle output such as cursor changes
-            }
-            _ => (),
-        }
-    });
-}
+//     fn title(&self) -> String {
+//         String::from("My App")
+//     }
+
+//     fn update(&mut self, message: Self::Message) {
+//         match message {
+//             MyMessage::Button1Clicked => {
+//                 println!("Button 1 clicked");
+//             }
+//             MyMessage::Button2Clicked => {
+//                 // Do nothing
+//             }
+//         }
+//     }
+
+//     fn view(&mut self) -> Element<Self::Message> {
+//         Column::new()
+//             .push(Button::new(&mut self.button_state1, Text::new("Button 1")).on_press(MyMessage::Button1Clicked))
+//             .push(Button::new(&mut self.button_state2, Text::new("Button 2")).on_press(MyMessage::Button2Clicked))
+//             .into()
+//     }
+// }
+
+// fn main() -> iced::Result {
+//     // MyApp::run(Settings::default())
+// }
