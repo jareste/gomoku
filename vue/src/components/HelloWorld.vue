34dee072d35d78124dccdd0b51c60615f0d0e4fa
bielaltes <bielaltesgrifoll@gmail.com>
2024-02-21 11:01:21 +0100

    vue working

---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+import init, { add , Car} from 'rust';
+init().then(() => {
+  console.log('init wasm-pack');
+  console.log('Add from vue:' + add(5, 6));
+  let c = Car.new();
+  console.log(c.number);
+  c.set_number(2);
+  console.log(c.number);
+});
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
